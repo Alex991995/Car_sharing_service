7ec2d8c630e274228398b8d4d59e249decc45e4c
Aleksandr Kisel <51911663+Alex991995@users.noreply.github.com>
2025-04-30 21:40:57 +0300

    feat: add basic stacture

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const { sequelize } =  require('./models') ;
-// const express =  require('express') ;
+const express =  require('express') ;
 
-// const app = express()
-// app.use(express.json())
+const app = express()
+app.use(express.json())
 
 
-// app.listen({port: 5000}, main)
+app.listen({port: 5000}, main)
 
 async function main() {
-  // console.log('serves is runnig on port'+ 5000)
+  console.log('serves is runnig on port'+ 5000)
   await sequelize.authenticate();
-  // console.log('database is sync')
+  console.log('database is sync')
 }
 main()
